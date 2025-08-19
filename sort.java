81bc9453b40a2e96718b24915c572233294ac61c
harishmas123 <162159461+harishmas123@users.noreply.github.com>
2025-08-19 23:27:59 +0530

    Create sort.java
    Write a program in java to find if a given integer x appears more than n/2 times in a sorted array of n integers.

---
+++ sort.java
@@ -0,0 +1,39 @@
+Write a program in java to find if a given integer x appears more than n/2 times in a sorted array of n integers.
+
+  import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int max=0,count=1,data=0;
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        for(int i=0;i<n-1;i++){
+            if(a[i]==a[i+1]){
+                count++;
+            }
+            else{
+                if(max<count){
+                    max=count;
+                    data=a[i];
+                }
+                count=1;
+                
+            }
+        }
+        if(max>n/2){
+            System.out.print("The majority element is : "+ data);
+        }
+        else{
+            System.out.print("No majority element found in the array");
+        }
+    }
+}
