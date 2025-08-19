b6d447bb988ba20d9736570ba7dc7ff7175d2f30
harishmas123 <162159461+harishmas123@users.noreply.github.com>
2025-08-19 23:24:17 +0530

    Create display.java
    Write a program to accept the elements into an array and display the array from right to left

---
+++ display.java
@@ -0,0 +1,24 @@
+Write a program to accept the elements into an array and display the array from right to left
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i=0;i<n;i++)
+        {
+            a[i] = sc.nextInt();
+        }
+        for(int i=n-1;i>=0;i--){
+            System.out.print(a[i] + " ");
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
