d5b1ba8c39b8c042c6f3cd0bb4e6a7e5f04b7dd2
harishmas123 <162159461+harishmas123@users.noreply.github.com>
2025-08-19 23:26:39 +0530

    Create replace.java
    Write a program to accept the elements into an array and replace every element in the array with the sum of left side element

---
+++ replace.java
@@ -0,0 +1,28 @@
+Write a program to accept the elements into an array and replace every element in the array with the sum of left side element
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        int sum = 0;
+        for(int i=0;i<n;i++)
+        {
+            a[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(sum +" ");
+            sum = sum + a[i];
+        }
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
