64cdec43903a62bbb31d6839ce4ac448382d6fcb
harishmas123 <162159461+harishmas123@users.noreply.github.com>
2025-08-19 23:25:29 +0530

    Create sum.java
    Write a program to accept the elements into an array and display the sum of the array.

---
+++ sum.java
@@ -0,0 +1,25 @@
+Write a program to accept the elements into an array and display the sum of the array.
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        int sum = 0;
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            sum = sum + a[i];
+        }
+        System.out.print(sum);
+        
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
